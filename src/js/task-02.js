c1159989e2a41b74f9f88108fd5d8bb5f81c6305
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-09 19:56:57 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const listEl = ingredients.map((elem) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = elem;
+  itemEl.classList.add("item");
+  return itemEl;
+});
+
+list.append(...listEl);
