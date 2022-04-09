0e193949e33416bf37b85ac62bf91a27bc9e5e77
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-09 20:29:18 +0300

    edit names

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,12 +7,12 @@ const ingredients = [
   "Condiments",
 ];
 
-const list = document.querySelector("#ingredients");
-const listEl = ingredients.map((elem) => {
+const listEl = document.querySelector("#ingredients");
+const itemsOfListEl = ingredients.map((elem) => {
   const itemEl = document.createElement("li");
   itemEl.textContent = elem;
   itemEl.classList.add("item");
   return itemEl;
 });
 
-list.append(...listEl);
+listEl.append(...itemsOfListEl);
