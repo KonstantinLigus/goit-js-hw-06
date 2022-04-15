0064c82953258c3b291cf39871573a73cf1e0ca5
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-15 19:45:13 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,2 +1,17 @@
 const textInput = document.querySelector("#validation-input");
-textInput.addEventListener("blure", onTextInputChange);
+textInput.addEventListener("blur", onTextInputChange);
+
+function onTextInputChange(event) {
+  console.log(event.currentTarget.value.length);
+  console.log(Number(event.currentTarget.dataset.length));
+  if (
+    event.currentTarget.value.length ===
+    Number(event.currentTarget.dataset.length)
+  ) {
+    event.currentTarget.classList.remove("invalid");
+    event.currentTarget.classList.add("valid");
+  } else {
+    event.currentTarget.classList.remove("valid");
+    event.currentTarget.classList.add("invalid");
+  }
+}
