d28cb11978a2682f727a68d90908acd772c87963
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-15 22:50:14 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormChange);
+function onFormChange(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be fill");
+  }
+  console.log({
+    value: email.value,
+    password: password.value,
+  });
+}
