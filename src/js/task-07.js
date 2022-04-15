2f768b164cd9edcfd286a85483f624a183f0662e
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-15 21:05:08 +0300

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,4 +2,6 @@ const inputRange = document.querySelector("input#font-size-control");
 const text = document.querySelector("span#text");
 inputRange.addEventListener("input", onInputRangeChange);
 
-function onInputRangeChange(event) {}
+function onInputRangeChange(event) {
+  text.style.fontSize = event.currentTarget.value + "px";
+}
