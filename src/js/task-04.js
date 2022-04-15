240eb048bdfcb364ef550f83c0127f283e4e6c94
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-15 17:41:05 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,21 +1,18 @@
 let counterValue = 0;
-
 const count = document.querySelector("#value");
-count = count.textContent = counterValue; 
-const decrementBtn = document.querySelector("[data-action="decrement"]");
-const incrementBtn = document.querySelector("[data-action="increment"]");
 
-decrementBtn.addEventListener('click', onDecrementBtnClick);
-incrementBtn.addEventListener('click', onIncrementBtnClick);
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+decrementBtn.addEventListener("click", onDecrementBtnClick);
+incrementBtn.addEventListener("click", onIncrementBtnClick);
 
 function onDecrementBtnClick() {
   counterValue -= 1;
-   
+  count.textContent = counterValue;
 }
 
 function onIncrementBtnClick() {
   counterValue += 1;
-   
+  count.textContent = counterValue;
 }
-
-
