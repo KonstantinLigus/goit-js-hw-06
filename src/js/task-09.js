a02a4d9575b64fe5465f0a567781d8e1c836ecc0
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-16 19:42:27 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector("button.change-color");
+const spanColor = document.querySelector("span.color");
+changeColorBtn.addEventListener("click", onChangeColorBtnClick);
+function onChangeColorBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
