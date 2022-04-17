53ab8928c7c23dd13ea2df10043a706cf5253d48
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-17 12:37:12 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,6 +3,45 @@ function getRandomHexColor() {
 }
 
 const createBtn = document.querySelector("[data-create]");
-createBtn.addEventListener("click", onCreateBtnClick);
+const destroyBtn = document.querySelector("[data-destroy]");
+const inputNumber = document.querySelector("input");
+const boxes = document.querySelector("div#boxes");
 
-function onCreateBtnClick() {}
+createBtn.addEventListener("click", onCreateBtnClick);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function onCreateBtnClick() {
+  createBoxes(Number(inputNumber.value));
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+// ---------
+// Variant 1
+function createBoxes(amount) {
+  let divArr = "";
+
+  for (let i = 0; i < amount; i += 1) {
+    divArr += `<div style='width: ${30 + i * 10}px; height: ${
+      30 + i * 10
+    }px; background-color: ${getRandomHexColor()}'></div>`;
+  }
+
+  boxes.insertAdjacentHTML("beforeend", divArr);
+}
+
+// Variant 2
+// function createBoxes(amount) {
+//   let divArr = [];
+//   for (let i = 0; i < amount; i += 1) {
+//     let div = document.createElement("div");
+//     div.style.width = `${30 + i * 10}px`;
+//     div.style.height = `${30 + i * 10}px`;
+//     div.style.backgroundColor = getRandomHexColor();
+//     divArr.push(div);
+//   }
+//   boxes.append(...divArr);
+// }
+// -----------
